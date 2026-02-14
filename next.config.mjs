5859bdc7e05551621a14c6bb27680b30ec735a75
Jack <bhargavjoshi1237@gmail.com>
2026-02-15 01:47:59 +0530

    feat: Implement Next.js rewrites for notes and assets, and add new logo assets.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/notes',
+        destination: 'https://geigernotes.vercel.app/notes',
+      },
+      {
+        source: '/notes/:path*',
+        destination: 'https://geigernotes.vercel.app/notes/:path*',
+      },
+      {
+        source: '/assets',
+        destination: 'https://geigerassets.vercel.app/assets',
+      },
+      {
+        source: '/assets/:path*',
+        destination: 'https://geigerassets.vercel.app/assets/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
